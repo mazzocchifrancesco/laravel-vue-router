ca9556aee07de1372e2d34898dc0fcab842ea67b
mazzocchifrancesco <123@email.it>
2024-02-08 10:28:58 +0100

    add img

--- src/components/AppEventList.vue
+++ src/components/AppEventList.vue
@@ -7,6 +7,12 @@ export default {
         return {
             store
         }
+    },
+    methods: {
+        //recupera immagini
+        getImage(img) {
+            return new URL(img, import.meta.url).href
+        },
     }
 }
 </script>
@@ -23,15 +29,17 @@ export default {
                         <div class="card-header">{{ evento.date }}</div>
 
                         <div class="card-body">
+                            <div class="imgBoxIndex rounded">
+                                <img class="cardImg rounded" :src="getImage(evento.image)" alt=evento.image>
+                            </div>
                             <h5 class="card-title">{{ evento.name }}</h5>
                             <h6 class="card-subtitle mb-2 text-muted">{{ evento.user.name }}</h6>
                             <p class="card-text">Restano <b>{{ evento.available_tickets }}</b> biglietti disponibili.
                             </p>
                         </div>
                         <div class="card-footer text-center">
-                            <a href="#" class="btn btn-primary">
-                                <font-awesome-icon icon="fa-solid fa-circle-info" class="me-2" />
-                                <span>Go somewhere</span>
+                            <a href="#" class=" btn-primary">
+                                <font-awesome-icon icon="fa-solid fa-circle-info fa-2xl" class="" />
                             </a>
                         </div>
                     </div>
